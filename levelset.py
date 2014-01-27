a24705d65b5c160ea348a1da09bf31c2f61db4ed
Clemens Verhoosel <c.v.verhoosel@tue.nl>
2014-01-27 14:51:02 +0100

    Updated levelset module

--- levelset.py
+++ levelset.py
@@ -1,7 +1,16 @@
-from finity import *
+from nutils import *
 
 @util.withrepr
-def SchwarzP ( ndims, L ):
+def SchwarzP ( ndims, L, t, inverse ):
   assert ndims in [2,3] and L > 0.
   L = float(L)
-  return lambda x : function.cos(numpy.pi*x[0]/L)+function.cos(numpy.pi*x[1]/L)+1 if ndims==2 else function.cos(numpy.pi*x[0]/L)+function.cos(numpy.pi*x[1]/L)+function.cos(numpy.pi*x[2]/L)
+  lvl = lambda x : function.cos(2.*numpy.pi*x[0]/L)+function.cos(2.*numpy.pi*x[1]/L)-1-t if ndims==2 else function.cos(2.*numpy.pi*x[0]/L)+function.cos(2.*numpy.pi*x[1]/L)+function.cos(2.*numpy.pi*x[2]/L)-t
+  return lvl if not inverse else lambda x: -lvl(x)                  
+
+@util.withrepr
+def Gyroid ( L, t, inverse ):
+  L = float(L)
+  lvl = lambda x : function.sin(2.*numpy.pi*x[0]/L)*function.cos(2.*numpy.pi*x[1]/L) + \
+                   function.sin(2.*numpy.pi*x[1]/L)*function.cos(2.*numpy.pi*x[2]/L) + \
+                   function.sin(2.*numpy.pi*x[2]/L)*function.cos(2.*numpy.pi*x[0]/L) - t
+  return lvl if not inverse else lambda x: -lvl(x)                  
